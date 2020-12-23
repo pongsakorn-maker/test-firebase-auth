b4a8fe9dc7615283e7ee20b4147f6428e8a1f0ab
pongsakorn-maker <pongsakorn.reo@gmail.com>
2020-12-23 14:07:34 +0700

    able to authen with firebase-auth

--- firebase-auth/src/components/LoginForm.js
+++ firebase-auth/src/components/LoginForm.js
@@ -1,23 +1,60 @@
 import React from 'react'
+import auth from '../firebase'
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+      currentUser: null,
+      message: ''
+    }
+  }
+
+  onChange = e => {
+    const { name, value } = e.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+
+    const { email, password } = this.state
+    // TODO: implement signInWithEmailAndPassword()
+  }
+
   render() {
     return (
       <section className="section container">
         <div className="columns is-centered">
           <div className="column is-half">
-            <form>
+            <form onSubmit={this.onSubmit}>
               <div className="field">
                 <label className="label">Email</label>
                 <div className="control">
-                  <input className="input" type="email" name="email" />
+                  <input
+                    className="input"
+                    type="email"
+                    name="email"
+                    onChange={this.onChange}
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <label className="label">Password</label>
                 <div className="control">
-                  <input className="input" type="password" name="password" />
+                  <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    onChange={this.onChange}
+                  />
                 </div>
               </div>
 
