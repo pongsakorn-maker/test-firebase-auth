47b64e222debffeddb012fed8d35b0a5028bd426
pongsakorn-maker <pongsakorn.reo@gmail.com>
2020-12-23 14:04:28 +0700

    add firebase authentication

---
+++ firebase-auth/src/firebase/config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyByT1iVknOPfk6gPE9gZpbr-3y43aX0Sic",
+    authDomain: "fir-auth-86bb6.firebaseapp.com",
+    projectId: "fir-auth-86bb6",
+    storageBucket: "fir-auth-86bb6.appspot.com",
+    messagingSenderId: "184372039713",
+    appId: "1:184372039713:web:9fb91952525049f4a09360",
+    measurementId: "G-EGPGC1EMC4"
+  };
